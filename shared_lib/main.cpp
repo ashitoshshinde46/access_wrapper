993b2d737e78d8d852b542a5425ea78b2e6192b8
ashitoshshinde46 <ashitoshshinde46@gmail.com>
2023-12-13 15:52:42 +0530

    test main changes

--- shared_lib/main.cpp
+++ shared_lib/main.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main(){
 
+uint8_t Data[4] = { 0xff, 0xff, 0xff, 0xff };
+int error=dio_readall(Data);
+cout<<"Error :"<<error<<endl;
+cout<<"Port data:"<< Data[0]<<endl;   
 return 0;
 }
